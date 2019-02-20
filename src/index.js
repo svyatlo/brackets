5b9f6875b9568845e5f07166fa652a61a8744d83
svyatlo <svyatlo@list.ru>
2019-02-21 00:20:50 +0300

    brackets are solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  function change(element) {
+    return element.replace(',', '')
+  }
+
+  function isStrHasPattern(str, arrayOfPatterns) {
+    return arrayOfPatterns.some(pattern => str.includes(pattern));
+  }
+
+  let stringConfig = bracketsConfig.map(String).map(change);
+  let strHasPattern = isStrHasPattern(str, stringConfig);
+
+  while (strHasPattern) {
+    stringConfig.forEach((pattern) => {
+      if (str.includes(pattern)) {
+        str = str.replace(pattern, '');
+      }
+    });
+
+    strHasPattern = isStrHasPattern(str, stringConfig);
+  }
+
+  return str.length === 0;
 }
